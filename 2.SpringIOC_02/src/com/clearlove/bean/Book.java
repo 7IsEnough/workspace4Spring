8a9f1280d08a83937eade31223e05bdedfb4111e
Promise <976949689@qq.com>
2021-03-01 22:15:39 +0800

    2021年3月1日22:15:33

---
+++ 2.SpringIOC_02/src/com/clearlove/bean/Book.java
@@ -0,0 +1,44 @@
+package com.clearlove.bean;
+
+/**
+ * @author promise
+ * @date 2021/2/21 - 13:44
+ */
+public class Book {
+
+  private String bookName;
+  private String author;
+
+  public void myInit() {
+    System.out.println("这是图书的初始化方法...");
+  }
+
+  public void myDestory() {
+    System.out.println("这是图书的销毁方法....");
+  }
+
+  public Book() {
+    System.out.println("book被创建..");
+  }
+
+  public String getBookName() {
+    return bookName;
+  }
+
+  public void setBookName(String bookName) {
+    this.bookName = bookName;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+
+  @Override
+  public String toString() {
+    return "Book{" + "bookName='" + bookName + '\'' + ", author='" + author + '\'' + '}';
+  }
+}
---
+++ 2.SpringIOC_02/src/com/clearlove/com/clearlove/test/IOCTest.java
@@ -0,0 +1,28 @@
+package com.clearlove.com.clearlove.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author promise
+ * @date 2021/3/1 - 22:01
+ */
+public class IOCTest {
+
+    ConfigurableApplicationContext ioc = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    /**
+     * 单例：Bean的生命周期
+     *      (容器启动)构造器 --> 初始化方法 --> (容器关闭)销毁方法
+     * 多实例：
+     *      获取bean(构造器 --> 初始化方法) --> 容器关闭不会调用bean的销毁方法
+     */
+    @Test
+    public void test() {
+        Object bean = ioc.getBean("book01");
+        System.out.println("容器关闭了.....");
+        ioc.close();
+    }
+}
